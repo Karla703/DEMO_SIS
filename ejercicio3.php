b052cbc52fdb124c0198ff3285a3a6b43d7ea1d6
Karla703 <64883489+Karla703@users.noreply.github.com>
2020-05-20 21:31:22 -0600

    Create ejercicio3.php
    unnuevo parrafo

---
+++ ejercicio3.php
@@ -0,0 +1,48 @@
+﻿<?php
+ $resultado1 = "";
+ $resultado2 = "";
+
+    if (isset($_POST["txtbase"])) {
+
+        $proceso = ($_POST["txtbase"]);
+        $proceso2= ($_POST["txtaltura"]);
+        $resultado1 = ($proceso * 2)+ ($proceso2 * 2);
+
+        $resultado2 = ($_POST["txtbase"]) * ($_POST["txtaltura"]);
+
+       
+      
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+<p> no entiendo lo que estoy haciendo </p>
+    <h2>Mostrar en pantalla el área y perimetro de un rectángulo</h2>
+    <hr>
+
+    <form action="ejercicio3.php" method="post">
+    <label for="txtbase"> Digite la base: </label>
+        <input type="text" name="txtbase" id="txtbase" required>
+        <hr>
+        <label for="txtaltura"> Digite la altura: </label>
+        <input type="text" name="txtaltura" id="txtaltura" required>
+        <hr>
+        <input type="submit" value="Calcular">
+    </form>
+
+    <?php
+    echo "El perimetro es : $resultado1 <hr>"; 
+    echo "El área es : $resultado2 cm <sup>2</sup>";
+    ?>
+    
+</body>
+</html>
